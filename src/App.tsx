15b407ed554c4a202dd31f233b3dabea9707292b
wriozumi <tshatmanaliev@gmail.com>
2022-06-06 14:56:43 +0200

    Code-split each route to have its own bundle

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import "./styles.css";
-import HomePage from "./scenes/HomePage";
-import VideoGame from "./scenes/VideoGame";
+import { Suspense, lazy } from 'react';
 import { Routes, Route } from "react-router-dom";
+
+const HomePage = lazy(() => import('./scenes/HomePage'));
+const VideoGame = lazy(() => import('./scenes/VideoGame'));
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/game" element={<VideoGame />} />
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/game" element={<VideoGame />} />
+      </Routes>
+    </Suspense>
   );
 }
